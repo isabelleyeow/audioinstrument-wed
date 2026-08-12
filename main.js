ee532eaec77f89bdd926536e72fa09401ed47ec6
isabelleyeow <s4160554@student.rmit.edu.au>
2026-08-12 14:56:32 +1000

    added audio inti
    added an event listener on dialog close to start the audio system

--- main.js
+++ main.js
@@ -8,14 +8,24 @@ const introDialogCloseButton = document.getElementById("intro-dialog-close");
 // find our test button
 const testButton = document.getElementById('test-button');
 // init our synth
-const synth = new Tone.Synth().toDestination();
+const synth = new Tone.Synth();
 
 //// dialog
+// show dialog on page load
 introDialog.showModal();
+// close dialog when user clicks
 introDialogCloseButton.addEventListener("click", function closeIntroDialog(){
     introDialog.close();
 });
+// whenever dialog closes, intialise the audio system
+introDialog.addEventListener("close", toneInit);
 
+
+//// tone
+// run to setup our audio system
+function toneInit(){
+    synth.connect(Tone.Destination);
+}
 
 // do something when this button is clicked 
 testButton.addEventListener("click", playNote);
