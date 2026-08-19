553f8f3aa0fcc5b4fe61d9c758f3a52276ec8682
isabelleyeow <s4160554@student.rmit.edu.au>
2026-08-19 15:58:35 +1000

    css and js edge case
    added in some visual feedback and handled edge cases for key press

--- main.js
+++ main.js
@@ -12,6 +12,16 @@ const key = document.getElementById("key-test");
 // init our synth
 // change key to poly synth
 const synth = new Tone.PolySynth();
+// is the user currently holding down the key
+let mouseButtonHeld = false;
+// if user holds down key, set to true then if they let it up set to false
+window.addEventListener("mousedown", function() {
+    mousedButtonHeld = true;
+
+});
+window.addEventListener("mouseup", function(){
+    mouseButtonHeld = false;
+});
 
 //// dialog
 // show dialog on page load
@@ -65,5 +75,13 @@ function endNote(e){
 key.addEventListener("mousedown", startNote);
 key.addEventListener("mouseup", endNote);
 key.addEventListener("mouseleave", endNote);
+// if user is holding mouse button down when entering the key, play note
+key.addEventListener("mouseenter", function(e){
+    if (mouseButtonHeld === true){
+        startNote(e);
+    }
+});
+
+
 // key.addEventListener("click", playDataNote);
 // testButton.addEventListener("click", playDataNote);
